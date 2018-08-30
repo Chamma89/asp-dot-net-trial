446e3ab45eea1950420c7a74f46162dfd5ed9b3a
fouad <Chamma.fouad@gmail.com>
2018-08-30 22:32:14 +1000

    added Contexts folder for Database, a few errors in Startup.cs file

---
+++ DemoASP/DemoASP/Contexts/DemoDBContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using DemoASP.Models;
+namespace DemoASP.Contexts
+{
+    public class DemoASPDBContext : DbContext
+    {
+        public DemoASPDBContext(DbContextOptions<DemoASPDBContext> options) : base(options) { }
+
+        public DbSet<Group> Groups { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
--- DemoASP/DemoASP/Startup.cs
+++ DemoASP/DemoASP/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -33,6 +34,8 @@ namespace DemoASP
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<DemoASPDBContext>(options => options.UseNpgsql(Configuration.GetConnectionString("Default")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
